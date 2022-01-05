9865da7c57f2ff62a0c0aaae310d031dbe4969b1
Ginny <ginnynjon@gmail.com>
2022-01-05 10:39:01 +0000

    account statement output is as desired except date order, test failing based on object type

--- BankTechTest/lib/account.rb
+++ BankTechTest/lib/account.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class Account
-  attr_reader :transactions
+  attr_reader :transactions, :balance
 
   def initialize
     @transactions = []
+    @balance = 0
   end
 
   def deposit(credit, date)
@@ -13,7 +14,29 @@ class Account
 
   def withdrawal(debit, date)
     @transactions << [-debit, date]
+  end
 
+  def statement_header
+    puts "\n""   date    || credit || debit || balance"
+  end
+
+  def account_statement
+    statement_header
+    balance = 0
+   # p by_date_newest_first = @transactions.reverse { |x, y| x["date"] <=> y["date"] }
+   #  p by_date_oldest_first = by_date_newest_first.reverse { |x, y| x["date"] <=> y["date"] }
+   #
+   #  by_date_oldest_first.each { |amount, date| by_date_oldest_first << (balance += amount) }
+   #  p by_date_oldest_first
+  #   by_date = @transactions.reverse { |x, y| x["date"] <=> y["date"] }
+  #   p by_date
+    @transactions.map { |amount, date|
+      if amount >= 0
+        puts "#{date} ||  #{amount}  ||       || #{balance += amount}"
+      else
+        puts "#{date} ||        ||  #{-amount}  ||  #{balance += amount}"
+      end
+     }
   end
 
 end
--- BankTechTest/spec/account_spec.rb
+++ BankTechTest/spec/account_spec.rb
@@ -19,4 +19,15 @@ describe 'account' do
       expect(account.transactions).to include([-500, '14-01-2023'])
     end
   end
+
+  describe 'account statement' do
+    it 'prints out a list of transactions and balance after each transaction' do
+      account.deposit(1000, '10-01-2023')
+      account.deposit(2000, '13-01-2023')
+      account.withdrawal(500, '14-01-2023')
+      expect(account.account_statement).to include('balance')
+      # may need to do this as a feature test - result is nil, nil, nil
+      # printed result is correct except date order
+    end
+  end
 end
